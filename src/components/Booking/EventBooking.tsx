a4b1a44e4edb2a5a739f4a6470cd1f9c8f135340
johnaribatise1217 <johnaribatise@hsass.org>
2026-08-24 01:10:23 +0100

    fixed index page image

--- src/components/Booking/EventBooking.tsx
+++ src/components/Booking/EventBooking.tsx
@@ -553,7 +553,7 @@ const BookingForm: React.FC<BookingFormProps> = ({ onNext, onPrevious }) => {
                     <h3 className="font-bold text-lg mb-3 bricolage-grotesque">Booking Note!</h3>
                     <p className="text-sm text-gray-700 mb-4 inter">
                         Hey Love, Ms. Pepo will not be taking bookings during <b className='font-bold'>August and September 2026.</b> <br /> Bookings are allowed from <b>October 1, 2026</b> onward. To request a date or ask about availability, please email{" "}
-                        <a href="mailto:info.mspepo@gmail.com" className="text-blue-600 underline">info.mspepo@gmail.com</a>.
+                        <a href="mailto:info.mspepo@gmail.com" className="text-blue-600 underline">info.mspepo@gmail.com</a>. <br />
                         Thank you , I look forward to working with you.
                     </p>
                 </div>
